bd095cf2d6f9d2cadc5d833843271569a06b8101
Tomasz Cichoń <mrciastexx@gmail.com>
2017-07-04 13:01:44 +0200

    Import from old organization repository.

---
+++ Missouri/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OverCR.Missouri")]
+[assembly: AssemblyDescription("Missouri XML Configuration Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OverCR Systems")]
+[assembly: AssemblyProduct("OverCR.Missouri")]
+[assembly: AssemblyCopyright("Copyright © OverCR Systems 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4973ceca-7d90-4f9c-8b9a-f1a798bcbe71")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.22.5803")]
+[assembly: AssemblyFileVersion("1.0.22.5803")]
---
+++ README.md
@@ -0,0 +1,1 @@
+<p align="center"><img src="https://github.com/OverCR-Systems/Missouri/blob/master/OverCR.Missouri/Logos/missouri-banner-512px.png"></img></p>
